8f4d046a570fbe37861b26aabb76f68c4316dc4c
MyName <prathamesh@collabint.com>
2024-10-08 18:02:44 +0530

    indivisual left and right both are working

--- module_4_assessment/module_4_assignment/src/wall_following.cpp
+++ module_4_assessment/module_4_assignment/src/wall_following.cpp
@@ -3,7 +3,8 @@
 #include <sensor_msgs/msg/imu.hpp>
 #include <chrono>
 
-float right_distance_wall_following = 3.00, threshold_value = 0.2;
+int rightFlag = 0, leftFlag = 1;
+float wallFollowingDistance = 3.00, threshold_value = 0.2;
 double Kp, Ki, Kd;
 float previous_error, integral, dt;
 
@@ -17,6 +18,10 @@ public:
         ImuSubscription_ = this->create_subscription<sensor_msgs::msg::Imu>("/imu_data/out", 10, std::bind(&WallFollowingNode::imu_callback, this, std::placeholders::_1));
         // Publisher for velocity commands
         velocity_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+        // timer_ = this->create_wall_timer(
+        // std::chrono::milliseconds(10), // Call every 100ms (10 Hz)
+        // std::bind(&WallFollowingNode::PID_wallFollowing, this));
         Kp = 0.3;
         Ki = 0.0;
         Kd = 3.0;
@@ -35,40 +40,115 @@ private:
     }
     void wall_callback(const geometry_msgs::msg::Twist msg)
     {
-        auto cmd_msg = geometry_msgs::msg::Twist();
+        wallDistance.linear.x = msg.linear.x;
+        wallDistance.linear.y = msg.linear.y;
+        wallDistance.linear.z = msg.linear.z;
 
-        // Calculate error
-        double error = right_distance_wall_following - msg.linear.z;
-
-        // Calculate PID terms
-        double P = Kp * error;
-        integral += Ki * error * dt;
-        double D1 = ((error - previous_error) / dt);
-        double D = Kd * D1;
-
-        // PID output
-        double pid_output = P + integral + D;
-        // Restrict to max/min
-        if (pid_output > 1)
-            pid_output = 1;
-        else if (pid_output < -1)
-            pid_output = -1;
-
-        // Use IMU data to adjust the PID output (e.g., add some correction based on yaw)
-        double yaw_correction = -0.1 * imu_z; // Example correction
-        cmd_msg.angular.z = pid_output;       //+ yaw_correction;
-
-        // Set linear speed
-        cmd_msg.linear.x = 0.5; // Adjust as necessary
-
-        // Publish the command
-        velocity_publisher_->publish(cmd_msg);
-
-        // Update previous error
-        previous_error = error;
+        wallDistance.angular.x = msg.angular.x;
+        wallDistance.angular.y = msg.angular.y;
+        wallDistance.angular.z = msg.angular.z;
+        PID_wallFollowing();
     }
+
+    void PID_wallFollowing()
+    {
+        auto cmd_msg = geometry_msgs::msg::Twist();
+        if (rightFlag == 1)
+        {
+            if (wallDistance.linear.z < (wallFollowingDistance + frontThreshold_) && wallDistance.linear.z > (wallFollowingDistance - frontThreshold_))
+            {
+
+                // Calculate error
+                double error = wallFollowingDistance - wallDistance.linear.z;
+
+                // Calculate PID terms
+                double P = Kp * error;
+                integral += Ki * error * dt;
+                double D1 = ((error - previous_error) / dt);
+                double D = Kd * D1;
+
+                // PID output
+                double pid_output = P + integral + D;
+                // Restrict to max/min
+                if (pid_output > 1)
+                    pid_output = 1;
+                else if (pid_output < -1)
+                    pid_output = -1;
+
+                // Use IMU data to adjust the PID output (e.g., add some correction based on yaw)
+                double yaw_correction = -0.1 * imu_z; // Example correction
+                cmd_msg.angular.z = pid_output;       //+ yaw_correction;
+
+                // Set linear speed
+                cmd_msg.linear.x = 0.5; // Adjust as necessary
+
+                // Publish the command
+                velocity_publisher_->publish(cmd_msg);
+
+                // Update previous error
+                previous_error = error;
+            }
+            else
+            {
+                cmd_msg.linear.x = 0;
+                cmd_msg.linear.y = 0;
+                cmd_msg.linear.z = 0;
+
+                velocity_publisher_->publish(cmd_msg);
+            }
+        }
+        if (leftFlag == 1)
+        {
+            if (wallDistance.linear.y < (wallFollowingDistance + frontThreshold_) && wallDistance.linear.y > (wallFollowingDistance - frontThreshold_))
+            {
+
+                // Calculate error
+                double error = wallFollowingDistance - wallDistance.linear.y;
+
+                // Calculate PID terms
+                double P = Kp * error;
+                integral += Ki * error * dt;
+                double D1 = ((error - previous_error) / dt);
+                double D = Kd * D1;
+
+                // PID output
+                double pid_output = P + integral + D;
+                // Restrict to max/min
+                if (pid_output > 1)
+                    pid_output = 1;
+                else if (pid_output < -1)
+                    pid_output = -1;
+
+                // Use IMU data to adjust the PID output (e.g., add some correction based on yaw)
+                // double yaw_correction = -0.1 * imu_z; // Example correction
+                cmd_msg.angular.z = -pid_output;       //+ yaw_correction;
+
+                // Set linear speed
+                cmd_msg.linear.x = 0.5;
+
+                // Publish the command
+                velocity_publisher_->publish(cmd_msg);
+
+                // Update previous error
+                previous_error = error;
+            }
+            else
+            {
+                cmd_msg.linear.x = 0;
+                cmd_msg.linear.y = 0;
+                cmd_msg.linear.z = 0;
+
+                velocity_publisher_->publish(cmd_msg);
+            }
+        }
+    }
+
+    float frontThreshold_ = 2.00;
+
     // veriable declare
     float imu_x, imu_y, imu_z, imu_w;
+    geometry_msgs::msg::Twist wallDistance; // Declare wallDistance with its explicit type
+    rclcpp::TimerBase::SharedPtr timer_;
 
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr LidarSubscription_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr ImuSubscription_;
